189455e31f25effa1146d0ab303c4439e1d0bbe9
Aji <adele513900383@gmail.com>
2017-12-25 22:08:19 +0800

    更新 karma.macha；一些描述的更新

---
+++ KarmaTest/karma.mocha.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration
+module.exports = function (config) {
+  config.set({
+    // frameworks:     frameworks to use: available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    // reporters:      test results reporter to use
+    //                 possible values: 'dots', 'progress'(default): available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    // logLevel:       level of logging: possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    frameworks: ['mocha'],
+    reporters: ['kjhtml'],
+    logLevel: config.LOG_INFO,
+
+    // basePath:    base path that will be used to resolve all patterns (eg. files, exclude)
+    // files:       list of files / patterns to load in the browser
+    // exclude:     list of files to exclude
+    basePath: '../MochaTest/MochaTest-1',
+    files: ['**/*.js'],
+    exclude: [],
+
+    // port:          web server port
+    // colors:        enable / disable colors in the output (reporters and logs)
+    // autoWatch      enable / disable watching file and executing tests whenever any file changes
+    // browsers:      start these browsers, available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    port: 9876,
+    colors: true,
+    autoWatch: true,
+    browsers: ['Chrome'],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
--- README.md
+++ README.md
@@ -5,6 +5,3 @@ Web Test Demos. Include js test, html test and so on, like Jasmine, karma, mocha
 * Author: AjiMIde
 * 2017-12-01
 
-#### catalog 目录
-
-* [Karma](/tree/master/Karma)
